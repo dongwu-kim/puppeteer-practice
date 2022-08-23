87a50edf17b3fdf6b202b633eafe7c6a7eeaa5e8
KimDongwu <hugo@alphaworks.xyz>
2022-08-23 21:44:17 +0900

    final

--- index.ts
+++ index.ts
@@ -2,6 +2,12 @@ import * as puppeteer from "puppeteer";
 import dotEnv from "dotenv";
 import path from "path";
 
+import * as fs from "fs";
+
+const folderPath = __dirname + "/dojagi";
+
+import * as cheerio from "cheerio";
+
 (() => {
   const result = dotEnv.config({ path: path.join(__dirname, "/", ".env") });
   if (result.parsed == undefined)
@@ -11,14 +17,25 @@ import path from "path";
 
 const { TEST_URL } = process.env;
 
+interface ListData {
+  name: string;
+  description: string;
+  region: string;
+  member: number;
+  profile_link: string;
+  image_url: string;
+  cafe_link: string;
+}
+
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: {
       width: 1200,
       height: 800,
     },
   });
+
   const page = await browser.newPage();
   await page.goto(TEST_URL ?? "");
   await page.waitForXPath(
@@ -43,7 +60,80 @@ const { TEST_URL } = process.env;
     (await (await moreButtonNode[0].getProperty("href")).jsonValue()) as string
   );
 
-  const [sectionPagenation] = await page.$x(
-    '//*[@id="mainContainer"]/div/div[1]/div[3]/div/div[3]/div/div/div'
+  let listData: ListData[][] = [];
+  let paginationOffset = 1;
+
+  while (true) {
+    let currentPageList: ListData[] = [];
+
+    const test = await page.waitForXPath(
+      '//*[@id="mainContainer"]/div/div[1]/div[3]/div/div[3]/ul'
+    );
+
+    const url = page.url();
+    const content = await page.content();
+    const $ = cheerio.load(content);
+
+    const list = $(
+      "#mainContainer > div > div.SectionSearchContent > div.section_search_content > div > div.cafe_list_area > ul > li"
+    );
+
+    list.each((_, list) => {
+      const name = $(list).find(".cafe_name").text();
+      const description = $(list).find(".cafe_introduction").text();
+      let region = "";
+      let member = 0;
+      let profile_link = "";
+      const image_url = $(list).find(".thumbnail").attr("src") ?? "";
+      const cafe_link = $(list).find(".thumbnail_area").attr("href") ?? "";
+
+      $(list)
+        .find(".cafe_info_row")
+        ?.each((_, _list) => {
+          if ($(_list).find(".info_title").text() === "멤버수") {
+            member = Number($(_list).find(".info_data").text());
+          }
+
+          if ($(_list).find(".info_title").text() === "지역") {
+            region = $(_list).find(".info_data").text();
+          }
+
+          const profileContent = $(_list).find(".cafe_profile_link");
+          if (profileContent) {
+            profile_link = profileContent.attr("href") ?? "";
+          }
+        });
+
+      currentPageList.push({
+        name,
+        description,
+        region,
+        member,
+        profile_link,
+        image_url,
+        cafe_link,
+      });
+    });
+
+    if (
+      listData.length !== 0 &&
+      listData[listData.length - 1][0].name === currentPageList[0].name
+    ) {
+      console.log(listData.length);
+      break;
+    }
+
+    listData.push(currentPageList);
+
+    paginationOffset += 1;
+
+    await page.goto(`${url.split("&p=")[0]}&p=${paginationOffset}`);
+  }
+
+  fs.writeFileSync(
+    folderPath + "/crawling.ts",
+    `export const dojagiInfo = ${JSON.stringify(listData.flat(), null, 2)};`
   );
+
+  browser.close();
 })();
